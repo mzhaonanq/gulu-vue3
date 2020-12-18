78bc7b843c72453e5a5c6f20ac1bceb08a6673b6
mzhaonanq <1002604003@qq.com>
2020-12-18 21:24:40 +0800

    完成屏幕分辨率小于500px时logo居中

--- src/components/Topnav.vue
+++ src/components/Topnav.vue
@@ -8,37 +8,44 @@
   </div>
 </template>
 <script lang="ts">
-import {inject, Ref} from "vue";
+import {inject, Ref} from 'vue';
+
 export default {
-  name:"Topnav",
-  setup(){
-    const asideVisible = inject<Ref<boolean>>("asideVisible")
-    const toggleAsideVisible=()=>{
-      asideVisible.value = !asideVisible.value
-    }
-    return {toggleAsideVisible}
+  name: 'Topnav',
+  setup() {
+    const asideVisible = inject<Ref<boolean>>('asideVisible');
+    const toggleAsideVisible = () => {
+      asideVisible.value = !asideVisible.value;
+    };
+    return {toggleAsideVisible};
   }
 }
 </script>
 
 <style lang="scss" scoped>
-.topnav{
-  background:pink;
+.topnav {
+  background: pink;
   display: flex;
+  justify-content: center;
+  align-items: center;
   padding: 16px;
   position: relative;
   z-index: 10;
-  > .logo{
+  > .logo {
     margin-right: auto;
     max-width: 6em;
   }
-  > .menu{
+  > .menu {
     display: flex;
     white-space: nowrap;
     flex-wrap: nowrap;
-    > li{
+    > li {
       margin: 0 1em;
     }
+  }
+  @media (max-width: 500px) {
+    > .logo {margin: 0 auto;}
+    > .menu {display: none;}
   }
 }
 
