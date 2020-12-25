3cb09bae3341da007f5ea8ec5ba98b93374df9dc
mzhaonanq <1002604003@qq.com>
2020-12-25 22:04:53 +0800

    完成level样式

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -13,6 +13,22 @@
     <Button size="big" >我是按钮</Button>
     <Button >我是按钮</Button>
   </div>
+  <hr>
+  <div>
+    <Button level="main">我是按钮</Button>
+    <Button level="danger">我是按钮</Button>
+    <Button >我是按钮</Button>
+    <hr>
+    <Button theme="button" level="main">我是按钮</Button>
+    <Button theme="link" level="main">我是按钮</Button>
+    <Button theme="link" >我是按钮</Button>
+    <Button theme="text" level="main">我是按钮</Button>
+    <hr>
+    <Button theme="button" level="danger">我是按钮</Button>
+    <Button theme="link" level="danger">我是按钮</Button>
+    <Button theme="text" level="danger">我是按钮</Button>
+  </div>
+
 
 
 
