95763d0e10e54ed9aa54fe6b31cbf0fe658c81a5
mzhaonanq <1002604003@qq.com>
2020-12-29 14:17:22 +0800

    完成Dialog基本样式

--- src/lib/Dialog.vue
+++ src/lib/Dialog.vue
@@ -2,17 +2,13 @@
   <div class="gulu-dialog-overlay"></div>
   <div class="gulu-dialog-wrapper">
     <div class="gulu-dialog">
-    <header>标题</header>
+    <header>标题 <span class="gulu-dialog-close"></span></header>
     <main>
-      <p>
-        段落一
-      </p>
-      <p>
-        段落二
-      </p>
+      <p>段落一</p>
+      <p>段落二</p>
     </main>
     <footer>
-    <Button>OK</Button>
+    <Button level="main">OK</Button>
       <Button>Cancel</Button>
     </footer>
     </div>
@@ -25,3 +21,71 @@ export default {
   components:{Button}
 }
 </script>
+
+<style lang="scss">
+$radius: 4px;
+$border-color: #d9d9d9;
+
+.gulu-dialog-overlay{
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background: fade_out(black, 0.5);
+  z-index: 10;
+}
+.gulu-dialog-wrapper{
+  position: fixed;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+  z-index: 11;
+  > .gulu-dialog{
+    background: white;
+    border-radius: $radius;
+    box-shadow: 0 0 3px fade_out(black, 0.5);
+    min-width: 15em;
+    max-width: 90%;
+    > header{
+      padding: 12px 16px;
+      border-bottom: 1px solid $border-color;
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+      font-size: 20px;
+      > .gulu-dialog-close{
+        position: relative;
+        display: inline-block;
+        width: 16px;
+        height: 16px;
+        cursor: pointer;
+        &::before,
+        &::after {
+          content: '';
+          position: absolute;
+          height: 1px;
+          background: black;
+          width: 100%;
+          top: 50%;
+          left: 50%;
+        }
+        &::before {
+          transform: translate(-50%, -50%) rotate(-45deg);
+        }
+        &::after {
+          transform: translate(-50%, -50%) rotate(45deg);
+        }
+      }
+    }
+    > main {
+      padding: 12px 16px;
+    }
+    > footer {
+      border-top: 1px solid $border-color;
+      padding: 12px 16px;
+      text-align: right;
+    }
+  }
+}
+</style>
