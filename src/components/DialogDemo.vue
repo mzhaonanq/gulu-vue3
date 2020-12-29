b6c6a5ea001d9d7990d00dd877bb81da1fb089b1
mzhaonanq <1002604003@qq.com>
2020-12-29 13:41:55 +0800

    创建Dialog组件

--- src/components/DialogDemo.vue
+++ src/components/DialogDemo.vue
@@ -1,5 +1,13 @@
 <template>
   <div>
-    Dialog组件
+   <div>Dialog示例</div>
+    <h1>示例一</h1>
+    <Dialog/>
   </div>
 </template>
+<script>
+import Dialog from '../lib/Dialog.vue'
+export default {
+  components:{Dialog}
+}
+</script>
---
+++ src/lib/Dialog.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="gulu-dialog-overlay"></div>
+  <div class="gulu-dialog-wrapper">
+    <div class="gulu-dialog">
+    <header>标题</header>
+    <main>
+      <p>
+        段落一
+      </p>
+      <p>
+        段落二
+      </p>
+    </main>
+    <footer>
+    <Button>OK</Button>
+      <Button>Cancel</Button>
+    </footer>
+    </div>
+  </div>
+</template>
+
+<script>
+import Button from "./Button.vue";
+export default {
+  components:{Button}
+}
+</script>
