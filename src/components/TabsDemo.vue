0b3e6e6c8112ed9cef419bd27831d2a06e65840c
mzhaonanq <1002604003@qq.com>
2020-12-29 21:00:31 +0800

    创建Tabs和Tab组件

--- src/components/TabsDemo.vue
+++ src/components/TabsDemo.vue
@@ -1,3 +1,15 @@
 <template>
-  <div>Tabs组件</div>
+  <div>Tabs组件展示</div>
+  <Tabs>
+    <Tab>Tab组件</Tab>
+  </Tabs>
 </template>
+<script>
+import Tabs from '../lib/Tabs.vue'
+import Tab from '../lib/Tab.vue'
+export default {
+  components:{
+    Tabs,Tab
+  }
+}
+</script>
