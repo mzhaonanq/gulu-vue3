a728a539b8b4040290c562a7eb0547829cfc5599
mzhaonanq <1002604003@qq.com>
2020-12-31 16:40:34 +0800

    Template Refs usage inside v-for 完成动态设置indicator宽度

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -5,8 +5,9 @@
            v-for="(t,index) in titles" :key="index"
            :class="{'selected':t===selected}"
            @click="()=>changeSelected(t)"
-      >{{ t }}
-      </div>
+           :ref="el => { if (el) divs[index] = el }"
+      >{{ t }}</div>
+      <div class="gulu-tabs-nav-indicator" ref="indicator"></div>
     </div>
     <div class="gulu-tabs-content">
       <component
@@ -16,14 +17,23 @@
     </div>
   </div>
 </template>
-<script>
+<script lang="ts">
 import Tab from "./Tab.vue"
+import {onMounted, ref} from "vue";
 
 export default {
   props: {
     selected: String
   },
   setup(props, context) {
+    const divs = ref<HTMLDivElement[]>([])
+    const indicator = ref<HTMLDivElement>(null)
+    onMounted(()=>{
+      const navItems = divs.value
+      const result = navItems.filter(navItem=>navItem.classList.contains('selected'))[0]
+      const {width} = result.getBoundingClientRect()
+      indicator.value.style.width=width+'px'
+    })
     const defaults = context.slots.default()
     defaults.forEach(tag => {
       if (tag.type !== Tab) {
@@ -36,7 +46,7 @@ export default {
     const changeSelected = (newValue) => {
       context.emit("update:selected", newValue)
     }
-    return {defaults, titles, changeSelected}
+    return {defaults, titles, divs,indicator,changeSelected}
   }
 }
 </script>
@@ -49,6 +59,15 @@ $border-color: #d9d9d9;
     display: flex;
     color: $color;
     border-bottom: 1px solid $border-color;
+    position: relative;
+    &-indicator{
+      position: absolute;
+      height: 3px;
+      background: $blue;
+      left: 0;
+      bottom: -1px;
+      width: 100px;
+    }
     &-item {
       padding: 8px 0;
       margin: 0 16px;
