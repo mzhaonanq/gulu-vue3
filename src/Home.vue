df3bf8c04f4350fdfa42abc0224168105113dc85
mzhaonanq <1002604003@qq.com>
2021-01-01 22:22:46 +0800

    完善首页细节

--- src/Home.vue
+++ src/Home.vue
@@ -11,24 +11,39 @@
     </div>
   </div>
   <div class="features">
-  <svg class="icon" aria-hidden="true">
-    <use xlink:href="#icon-vue"></use>
-  </svg>
-  <svg class="icon" aria-hidden="true">
-    <use xlink:href="#icon-ts"></use>
-  </svg>
-  <svg class="icon" aria-hidden="true">
-    <use xlink:href="#icon-light"></use>
-  </svg>
+    <ul>
+      <li>
+        <svg class="icon" aria-hidden="true">
+          <use xlink:href="#icon-vue"></use>
+        </svg>
+        <h3>基于 Vue 3</h3>
+        <p>骄傲地使用了 Vue 3 Composition API</p>
+      </li>
+      <li>
+        <svg class="icon" aria-hidden="true">
+          <use xlink:href="#icon-ts"></use>
+        </svg>
+        <h3>基于 TypeScript</h3>
+        <p>源代码使用 TypeScript 书写（非严格检查）</p>
+      </li>
+      <li>
+        <svg class="icon" aria-hidden="true">
+          <use xlink:href="#icon-light"></use>
+        </svg>
+        <h3>代码易读</h3>
+        <p>每个组件的源代码都及其简洁</p>
+      </li>
+    </ul>
   </div>
 </template>
 
 <script lang="ts">
-import Topnav from "./components/Topnav.vue";
+import Topnav from './components/Topnav.vue';
+
 export default {
   name: 'Home',
   components: {Topnav},
-}
+};
 </script>
 
 <style lang="scss" scoped>
@@ -56,9 +71,41 @@ export default {
   }
 }
 .features{
-  > .icon{
-    height: 64px;
-    width: 64px;
+  margin:64px auto;
+  width: 400px;
+  @media(min-width:800px){
+    width:800px ;
   }
+  @media(min-width:1200px){
+    width:1200px ;
+  }
+  > ul{
+    display: flex;
+    flex-wrap: wrap;
+    > li{
+      width: 400px;
+      margin: 16px 0;
+      display:grid;
+      justify-content: start;
+      align-content: space-between;
+      grid-template-areas:
+          "svg h3"
+          "svg p";
+      grid-template-rows: 1fr auto;
+      grid-template-columns: 80px auto;
+      > .icon{
+        grid-area: svg;
+        height: 64px;
+        width: 64px;
+      }
+      > h3{
+        grid-area: h3;
+      }
+      > p{
+        grid-area: p;
+      }
+    }
+  }
+
 }
 </style>
