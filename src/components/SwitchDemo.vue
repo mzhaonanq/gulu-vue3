a5a73558e17f8563bd614c9cb57f21265cd01e7e
mzhaonanq <1002604003@qq.com>
2021-01-03 15:48:45 +0800

    更新SwitchDemo组件样式

--- src/components/SwitchDemo.vue
+++ src/components/SwitchDemo.vue
@@ -1,17 +1,33 @@
 <template>
-<div>
-  <h1>Switch 组件示例 </h1>
-  <div class="demo">
-    <h2>常规用法</h2>
-    <div class="demo-component">
-      <Switch v-model:checked-show="value1"/>
+  <div>
+    <h1>Switch 组件示例 </h1>
+    <div class="demo">
+      <h2>常规用法</h2>
+      <div class="demo-component">
+        <Switch v-model:checked-show="value1" />
+      </div>
+      <div class="demo-actions">
+        <Button>查看代码</Button>
+      </div>
+      <div class="demo-code">
+        <pre>&lt;Switch v-model:checked-show="value1" /&gt;</pre>
+      </div>
     </div>
-    <h2>支持disabled</h2>
+  </div>
+  <div>
+  <div class="demo">
+    <h2>支持 disabled </h2>
     <div class="demo-component">
       <Switch v-model:checked-show="value2" disabled/>
     </div>
+    <div class="demo-actions">
+      <Button>查看代码</Button>
+    </div>
+    <div class="demo-code">
+      <pre>&lt;Switch v-model:checked-show="value1" disabled /&gt;</pre>
+    </div>
   </div>
-</div>
+  </div>
 </template>
 <script lang="ts">
 import Switch from '../lib/Switch.vue';
@@ -31,4 +47,30 @@ export default {
 </script>
 <style lang="scss" scoped>
 
+$border-color: #d9d9d9;
+.demo {
+  border: 1px solid $border-color;
+  margin: 16px 0 32px;
+  > h2 {
+    font-size: 20px;
+    padding: 8px 16px;
+    border-bottom: 1px solid $border-color;
+  }
+  &-component {
+    padding: 16px;
+  }
+  &-actions {
+    padding: 8px 16px;
+    border-top: 1px dashed $border-color;
+  }
+  &-code {
+    padding: 8px 16px;
+    border-top: 1px dashed $border-color;
+    > pre {
+      line-height: 1.1;
+      font-family: Consolas, 'Courier New', Courier, monospace;
+      margin: 0;
+    }
+  }
+}
 </style>
