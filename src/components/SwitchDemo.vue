7bae1b81f09f90e9a4bdd97eca22249b22f92877
mzhaonanq <1002604003@qq.com>
2021-01-03 19:27:00 +0800

    移动文件位置

--- src/components/SwitchDemo.vue
+++ src/components/SwitchDemo.vue
@@ -4,9 +4,9 @@
   <Demo :component="SwitchCode2"/>
 </template>
 <script lang="ts">
-import SwitchCode1 from './SwitchCode1.vue';
-import SwitchCode2 from './SwitchCode2.vue';
-import Demo from './Demo.vue';
+import SwitchCode1 from '../code/SwitchCode1.vue';
+import SwitchCode2 from '../code/SwitchCode2.vue';
+import Demo from '../code/Demo.vue';
 
 export default {
   name: 'SwitchDemo',
