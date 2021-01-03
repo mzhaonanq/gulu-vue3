65809ee43860866df796f19f845cd5407d195bb7
mzhaonanq <1002604003@qq.com>
2021-01-03 19:25:24 +0800

    删除SwitchDemo多余的代码

--- src/components/SwitchDemo.vue
+++ src/components/SwitchDemo.vue
@@ -4,50 +4,15 @@
   <Demo :component="SwitchCode2"/>
 </template>
 <script lang="ts">
-import Switch from '../lib/Switch.vue';
-import Button from '../lib/Button.vue';
 import SwitchCode1 from './SwitchCode1.vue';
 import SwitchCode2 from './SwitchCode2.vue';
-import {ref} from 'vue';
 import Demo from './Demo.vue';
 
 export default {
   name: 'SwitchDemo',
-  components: {Demo, Switch, Button},
+  components: {Demo},
   setup() {
-    const value1 = ref(false);
-    const value2 = ref(false);
-
-    return {value1,value2,SwitchCode1,SwitchCode2};
+    return {SwitchCode1, SwitchCode2};
   }
 };
 </script>
-<style lang="scss" scoped>
-
-$border-color: #d9d9d9;
-.demo {
-  border: 1px solid $border-color;
-  margin: 16px 0 32px;
-  > h2 {
-    font-size: 20px;
-    padding: 8px 16px;
-    border-bottom: 1px solid $border-color;
-  }
-  &-component {
-    padding: 16px;
-  }
-  &-actions {
-    padding: 8px 16px;
-    border-top: 1px dashed $border-color;
-  }
-  &-code {
-    padding: 8px 16px;
-    border-top: 1px dashed $border-color;
-    > pre {
-      line-height: 1.1;
-      font-family: Consolas, 'Courier New', Courier, monospace;
-      margin: 0;
-    }
-  }
-}
-</style>
