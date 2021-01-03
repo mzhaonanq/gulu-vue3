fcbbd46cb7ad03f81bccecf4a98a7f869a2645d7
mzhaonanq <1002604003@qq.com>
2021-01-03 16:06:01 +0800

    抽离用户需要拷贝的Switch组件的代码

---
+++ src/components/SwitchCode1.vue
@@ -0,0 +1,14 @@
+<template>
+  <Switch v-model:checked-show="value1" />
+</template>
+<script lang="ts">
+import Switch from '../lib/Switch.vue';
+import {ref} from 'vue';
+export default{
+  components:{Switch},
+  setup() {
+    const value1 = ref(false);
+    return {value1};
+  }
+}
+</script>
---
+++ src/components/SwitchCode2.vue
@@ -0,0 +1,14 @@
+<template>
+  <Switch v-model:checked-show="value2" disabled />
+</template>
+<script lang="ts">
+import Switch from '../lib/Switch.vue';
+import {ref} from 'vue';
+export default{
+  components:{Switch},
+  setup() {
+    const value2 = ref(false);
+    return {value2};
+  }
+}
+</script>
