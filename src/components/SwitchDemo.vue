3201884083d0a83729dc84704ca050a08694a4d9
mzhaonanq <1002604003@qq.com>
2021-01-03 15:42:32 +0800

    Switch组件支持disabled

--- src/components/SwitchDemo.vue
+++ src/components/SwitchDemo.vue
@@ -1,17 +1,34 @@
 <template>
 <div>
-<Switch v-model:checked-show="value" />
+  <h1>Switch 组件示例 </h1>
+  <div class="demo">
+    <h2>常规用法</h2>
+    <div class="demo-component">
+      <Switch v-model:checked-show="value1"/>
+    </div>
+    <h2>支持disabled</h2>
+    <div class="demo-component">
+      <Switch v-model:checked-show="value2" disabled/>
+    </div>
+  </div>
 </div>
 </template>
 <script lang="ts">
-import Switch from '../lib/Switch.vue'
+import Switch from '../lib/Switch.vue';
+import Button from '../lib/Button.vue';
 import {ref} from 'vue';
+
 export default {
-  name:"SwitchDemo",
-  components:{Switch},
-  setup(){
-    const value = ref(false)
-    return{value}
+  name: 'SwitchDemo',
+  components: {Switch, Button},
+  setup() {
+    const value1 = ref(false);
+    const value2 = ref(false);
+
+    return {value1,value2};
   }
-}
+};
 </script>
+<style lang="scss" scoped>
+
+</style>
