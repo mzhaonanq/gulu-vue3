f1fb81be378f19d76fb66aba1d277fac8d6de368
mzhaonanq <1002604003@qq.com>
2021-01-03 20:50:00 +0800

    增加代码预览框的横向滚动条

--- src/code/Demo.vue
+++ src/code/Demo.vue
@@ -53,11 +53,11 @@ $border-color: #d9d9d9;
   &-code {
     padding: 8px 16px;
     border-top: 1px dashed $border-color;
-    overflow: auto;
     > pre {
       line-height: 1.1;
       font-family: Consolas, 'Courier New', Courier, monospace;
       margin: 0;
+      overflow: auto;
     }
   }
 }
